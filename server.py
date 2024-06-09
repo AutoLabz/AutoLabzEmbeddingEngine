fded36727958165e21c32f4541c61ccf9dad3042
Saurabh Khanal <Skhanal247@gmail.com>
2024-06-09 17:15:27 -0400

    BM25Encoding & Sparse Vectors

--- server.py
+++ server.py
@@ -2,33 +2,63 @@ import jwt, datetime, os
 from flask import Flask, request, jsonify
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
+from transformers import BertTokenizer
+from pinecone import Pinecone, ServerlessSpec
+from pinecone_text.sparse import BM25Encoder
+
 
 load_dotenv()
 server = Flask(__name__)
 
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
+pineconeObj = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
 
 @server.route('/embed', methods=['POST'])
 def embed():
     print("HIT PYTHON SERVICE!")
     car_data = request.json.get('car_data', [])
+    print(car_data)
+
+    embeddings = []
+    embeddings = model.encode(car_data)
+
+    print("The shape of the embeddings are ", embeddings.shape)
+
+    # Dense Embeddings are created at this point so sematic part is half done. Now to create sparse vectors. 
+
+    bm25 = BM25Encoder()
+    bm25.fit(car_data)
+
+    sparse_vectors = bm25.encode_documents(car_data)
+    for vector in sparse_vectors:
+        print(vector)
+  
+
     if not car_data:
         return jsonify({'error': 'No car data provided'}), 400
     
-    # Create a list of embeddings for each car's attributes
-    embeddings = []
-    for car in car_data:
-        car_embeddings = {}
-        for key, value in car.items():
-            # Ensure the value is a string for the embedding model
-            value_str = str(value)
-            embedding = model.encode([value_str])[0]
-            car_embeddings[key] = embedding.tolist()
-        embeddings.append(car_embeddings)
-    #print(embeddings)
 
-    return jsonify({'embeddings': embeddings})
+    #return jsonify({'embeddings': embeddings})
+    return None
+
+
+
+
+@server.route('/search', methods=['GET'])
+def search():
+
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=5005)
+
+
+
+
+'''
+  tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    all_tokens = [tokenizer.tokenize(sentence.lower()) for sentence in car_data]
+    print(all_tokens[0])
+
+'''
